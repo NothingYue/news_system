d6b10208dc96badf8b5754a811b1418b4e59e05c
nothing <1014138810@qq.com>
2021-03-17 22:33:52 +0800

    项目上传

---
+++ src/main/java/com/cwy/news/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.cwy.news.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {//你在用电脑吗
+
+}
---
+++ src/main/java/com/cwy/news/test.java
@@ -0,0 +1,18 @@
+package com.cwy.news;
+
+
+import com.cwy.news.mapper.UserMapper;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class test {
+
+    @Autowired
+    private UserMapper userMapper;
+
+}
+
